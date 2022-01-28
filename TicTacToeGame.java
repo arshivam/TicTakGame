d683e6d27db426c465611d8f7204fb6c41eca21c
arshivam <shivam.armarkar007@gmail.com>
2022-01-28 09:01:39 +0530

    Added :Empty tictak board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,11 @@
 package com.bridgelabz.tictacgame;
 
 public class TicTacToeGame {
-
+	public static  void TickTac() {
+        char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to tictaktoe game =");
+		TickTac();
+	}
 }
