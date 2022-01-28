ad8deb10633c0fef9e2623969ad57eb65b912aa6
arshivam <shivam.armarkar007@gmail.com>
2022-01-28 09:51:18 +0530

    Added :Code for Player and Computer player with letter X and O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,38 @@
 package com.bridgelabz.tictacgame;
+import java.util.*;
 
 public class TicTacToeGame {
+
 	public static  void TickTac() {
-        char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+		char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };    
+	}
+	public static  void TickTacLetter() {
+		char player =' ';
+		char computer =' ';
+
+		System.out.println("Enter the Letter X or O");
+		Scanner sc = new Scanner(System.in);
+		char letter=sc.next().charAt(0);
+		if (letter == 'X') {
+
+			player=letter;
+			computer='O';
+			System.out.println("Player's letter is X");
+			System.out.println("Computer's letter is O");
+		}
+		else if(letter == 'O') {
+			player=letter;
+			computer='X';
+			System.out.println("Player's letter is O");
+			System.out.println("Computer's letter is x");
+		}
+		else {
+			System.out.println("Error. You have to choose X or O only.");
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to tictaktoe game =");
 		TickTac();
+		TickTacLetter();
 	}
 }
